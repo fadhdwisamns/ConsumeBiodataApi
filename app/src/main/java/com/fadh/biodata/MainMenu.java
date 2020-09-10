4411b8ca58bfe9c0b4fd49a0ed9773bf658d23fa
fadhdwisamns <fadhdwisam@gmail.com>
2020-09-10 15:04:42 +0700

    get movie

--- app/src/main/java/com/fadh/biodata/MainMenu.java
+++ app/src/main/java/com/fadh/biodata/MainMenu.java
@@ -2,13 +2,25 @@ package com.fadh.biodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-
+    Button btnDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        btnDetail = findViewById(R.id.btnDetail);
+
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inten = new Intent(MainMenu.this, Register.class );
+                startActivity(inten);
+            }
+        });
     }
 }
